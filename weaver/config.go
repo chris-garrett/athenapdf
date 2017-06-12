9f046e29c43bcf8b2d25cf21a10fc7e16d451717
Chris Garrett <chris@nesteggs.ca>
2017-06-12 13:20:46 -0600

    remove abs path

--- weaver/config.go
+++ weaver/config.go
@@ -77,7 +77,7 @@ func NewEnvConfig() Config {
 		CloudConvert:       cloudconvert,
 		HTTPAddr:           ":8080",
 		AuthKey:            "arachnys-weaver",
-		AthenaCMD:          "/opt/athenapdf/bin/athenapdf -S",
+		AthenaCMD:          "athenapdf -S",
 		MaxWorkers:         10,
 		MaxConversionQueue: 50,
 		WorkerTimeout:      90,
